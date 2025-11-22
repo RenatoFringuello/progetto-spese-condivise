4be2b6219a786063085209b2f4e83fae21dd5576
RenatoFringuello <renatofringuello@hotmail.it>
2025-11-22 20:34:59 +0100

    spostate le variabili e migliorato il layout, reso dinamico il recupero delle view

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 import { select, innerJoin } from "./utils.js";
 
 $(document).ready(async function(){
-	
-   loadView()
+    loadView()
 });
 
 
@@ -12,6 +11,7 @@ async function loadView(){
     const views = await select('view', 'select a, b, c, d', true)
     // const spese = await select('spesa', 'select a, b, c, d', true)
     
+    
     // const data = innerJoin(views, spese, 'id_spesa', 'id_spesa')
     // const data = innerJoin(views, spese, 'id_spesa', 'id_spesa')
     
@@ -19,11 +19,13 @@ async function loadView(){
         const item_list = `
         <li class="list-item my-padding">
             <div class="description">
-            ${v.nome}
+                <h2>${v.get('nome')}</h2>
+                <h4>${v.get('descrizione')}</h4>
             </div>
             <div class="btn"><i class="fa-solid fa-trash"></i></div>
         </li>`
         
+        $('#home-views').append(item_list)
     });
 
 
